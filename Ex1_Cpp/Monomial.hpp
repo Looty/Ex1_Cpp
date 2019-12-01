7cc4712c0f40f4a7e7eb2da06ce9778a79ab340b
Kobi Lvovsky <lvovkobi@gmail.com>
2019-12-01 16:32:07 +0200

    monomial class header file.

--- Ex1_Cpp/Monomial.hpp
+++ Ex1_Cpp/Monomial.hpp
@@ -5,7 +5,23 @@
 //  Created by Kobi Lvovsky on 01/12/2019.
 //  Copyright © 2019 Erez Mizrahi. All rights reserved.
 //
-
 #pragma once
-#include <stdio.h>
 
+class Monomial {
+    friend class Polynomial;
+public:
+    Monomial(int=1, unsigned int=0);
+    ~Monomial();
+    void set_c(int=1);
+    void set_d(unsigned int=0);
+    int get_c();
+    unsigned int get_d();
+    static int get_count();
+
+private:
+    int c;
+    unsigned int d;
+    static int monom_count;
+
+
+};
