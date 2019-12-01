d3cfd2e8b9b3cc95552e7f14271f59fad07d704a
Kobi Lvovsky <lvovkobi@gmail.com>
2019-12-01 15:43:57 +0200

    added mono app and cpp

---
+++ Ex1_Cpp/Monomial.cpp
@@ -0,0 +1,9 @@
+//
+//  Monomial.cpp
+//  Ex1_Cpp
+//
+//  Created by Kobi Lvovsky on 01/12/2019.
+//  Copyright © 2019 Erez Mizrahi. All rights reserved.
+//
+
+#include "Monomial.hpp"
---
+++ Ex1_Cpp/Monomial.hpp
@@ -0,0 +1,11 @@
+//
+//  Monomial.hpp
+//  Ex1_Cpp
+//
+//  Created by Kobi Lvovsky on 01/12/2019.
+//  Copyright © 2019 Erez Mizrahi. All rights reserved.
+//
+
+#pragma once
+#include <stdio.h>
+
